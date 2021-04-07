e1b0fe47488d7ea4c91c2b7b741174c37f73b99c
Jon Kent <jonkent95@gmail.com>
2021-04-07 10:54:29 +0100

    mock everything

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,41 +1,43 @@
-
-from typing import Union
 import unittest
 
-import flask_unittest
-from _pytest import unittest
-from selenium.webdriver import Chrome, ChromeOptions
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-from app import app as create_app
+from unittest.mock import patch
+from app import routes
+from app.routes import SUBMISSION_FILE
 
 
-class TestFoo(flask_unittest.LiveTestCase):
-    driver: Union[Chrome, None] = None
-    std_wait: Union[WebDriverWait, None] = None
+class FakeRequest:
 
-    @classmethod
-    def setUpClass(cls):
-        # Initiate the selenium webdriver
-        options = ChromeOptions()
-        options.add_argument('--headless')
-        cls.driver = Chrome(options=options)
-        cls.std_wait = WebDriverWait(cls.driver, 5)
-
-    @classmethod
-    def tearDownClass(cls):
-        # Quit the webdriver
-        cls.driver.quit()
-
-    def test_foo_with_driver(self):
-        # Use self.driver here
-        # You also have access to self.server_url and self.app
-        # Example of using selenium to go to index page and try to find some elements (on a hypothetical app)
-        self.driver.get(self.server_url)
-        self.std_wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'Register')))
-        self.std_wait.until(EC.presence_of_element_located((By.LINK_TEXT, 'Log In')))
+    def __init__(self) -> None:
+        self.args = {}
 
 
-class test_endpoints()
+class FakeFileBytes:
+
+    def __init__(self, submission: bytes) -> None:
+        self.submission = submission
+
+    def read(self) -> bytes:
+        return self.submission
+
+
+class TestRoutes(unittest.TestCase):
+
+    @patch('app.routes.MetaWrapper')
+    @patch('app.routes.deliver')
+    @patch('app.routes.jsonify')
+    def test_deliver_dap(self, mock_jsonify, mock_deliver, mock_meta_wrapper):
+
+        filename = "test_filename"
+        submission_bytes = b'{"survey_id":"283"}'
+
+        mock_request = FakeRequest()
+        mock_request.args['filename'] = filename
+
+        mock_file_bytes = FakeFileBytes(submission_bytes)
+        mock_request.files = {SUBMISSION_FILE: mock_file_bytes}
+
+        routes.request = mock_request
+        routes.deliver_dap()
+
+        mock_deliver.assert_called()
+        mock_jsonify.assert_called_with(success=True)
