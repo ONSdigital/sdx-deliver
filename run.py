8f418e9e0fc1eaa43c8670c03e03297ed3e17f2d
TomHolroyd <tomholroyd96@gmail.com>
2021-05-20 11:34:09 +0100

    test

--- run.py
+++ run.py
@@ -8,27 +8,8 @@ from app import app, cloud_config
 logger = structlog.get_logger()
 
 
-# class Server(BaseApplication):
-#
-#     def __init__(self, application, options_dict=None):
-#         self.options = options_dict or {}
-#         self.application = application
-#         super().__init__()
-#
-#     def load_config(self):
-#         config = {key: value for key, value in self.options.items()
-#                   if key in self.cfg.settings and value is not None}
-#         for key, value in config.items():
-#             self.cfg.set(key.lower(), value)
-#
-#         cloud_config()
-#
-#     def load(self):
-#         return self.application
-
-
 if __name__ == '__main__':
     logger.info('Starting SDX Deliver')
     cloud_config()
-    os.system("uvicorn app.routes:app --host 0.0.0.0 --port 5000 --workers 5 --timeout-keep-alive 43200")
+    os.system("uvicorn app.routes:app --host 0.0.0.0 --port 5000 --workers 5")
     # uvicorn.run(app, host="0.0.0.0", workers=2, port=5000)
