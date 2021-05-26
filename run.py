358e4e689686b987128dd04bbb868e16bfa557d7
TomHolroyd <tomholroyd96@gmail.com>
2021-05-26 14:53:20 +0100

    testing fast-api

--- run.py
+++ run.py
@@ -5,8 +5,8 @@ from app import cloud_config
 
 logger = structlog.get_logger()
 
-
 if __name__ == '__main__':
     logger.info('Starting SDX Deliver')
     cloud_config()
-    os.system("uvicorn app.routes:app --host 0.0.0.0 --port 5000 --workers 2")
+    # os.system("uvicorn app.routes:app --host 0.0.0.0 --port 5000 --workers 2")
+    os.system("gunicorn app.routes:app -b :5000 -w 4 -k uvicorn.workers.UvicornWorker")
