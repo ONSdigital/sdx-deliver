51f7f7a6a44433484a58b82373675d6be98b7d50
TomHolroyd <tomholroyd96@gmail.com>
2021-06-22 15:09:24 +0100

    indented logs

--- app/logger.py
+++ app/logger.py
@@ -41,7 +41,7 @@ def logging_config():
             structlog.stdlib.add_logger_name,
             structlog.stdlib.PositionalArgumentsFormatter(),
             merge_contextvars,
-            structlog.processors.JSONRenderer(),
+            structlog.processors.JSONRenderer(indent=2, sort_keys=True),
         ],
     )
     bind_contextvars(app='SDX-Deliver')
