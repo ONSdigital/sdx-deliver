6b563e0bf701920f477a328c4306a81066ef5fe6
Jon Kent <jonkent95@gmail.com>
2021-08-19 17:54:30 +0100

    Feedback suffix (#51)

--- tests/test_meta_wrapper.py
+++ tests/test_meta_wrapper.py
@@ -87,7 +87,7 @@ class TestMetaWrapper(unittest.TestCase):
         meta_data.set_feedback(self.test_survey)
         actual = meta_data.get_description()
         self.assertEqual(expected, actual)
-        self.assertEqual(f'{filename}:ftp', meta_data.filename)
+        self.assertEqual(f'{filename}-feedback:ftp', meta_data.filename)
 
     def test_set_comments(self):
         filename = "c37a3efa-593c-4bab-b49c-bee0613c4fb2"
