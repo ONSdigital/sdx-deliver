fba66ecd39eb4ee0814435b3d8ce468ae54d260f
Jon Kent <jonkent95@gmail.com>
2022-11-10 17:47:39 +0000

    No actual delivery as this is for testing only

--- app/deliver.py
+++ app/deliver.py
@@ -16,17 +16,4 @@ def deliver(meta_data: MetaWrapper, data_bytes: bytes):
     Encrypts any unencrypted data, writes to the appropriate location within the outputs GCP bucket and notifies DAP
     via PubSub
     """
-    if meta_data.output_type == OutputType.SEFT:
-        encrypted_output = data_bytes
-    else:
-        logger.info("Encrypting output")
-        encrypted_output = encrypt_output(data_bytes)
-        encrypted_bytes = encrypted_output.encode()
-        meta_data.md5sum = hashlib.md5(encrypted_bytes).hexdigest()
-        meta_data.sizeBytes = len(encrypted_bytes)
-
-    logger.info("Storing to bucket")
-    path = write_to_bucket(encrypted_output, filename=meta_data.filename, output_type=meta_data.output_type)
-
-    logger.info("Sending DAP notification")
-    send_message(meta_data, path)
+    logger.info("No delivery required as this is an integration environment")
