0c819de7648ce702b6431450cbc2c205d168755c
Jon Kent <jonkent95@gmail.com>
2023-06-09 15:45:22 +0100

    Remove encryption

--- app/encrypt.py
+++ app/encrypt.py
@@ -10,12 +10,13 @@ def encrypt_output(data_bytes: bytes) -> str:
     Encrypts data using DAP public key (GPG)
     """
 
-    encrypted_data = CONFIG.GPG.encrypt(data_bytes, recipients=CONFIG.RECIPIENTS, always_trust=True)
-
-    if encrypted_data.ok:
-        logger.info("Successfully encrypted output")
-    else:
-        logger.error("Failed to encrypt output")
-        logger.error(encrypted_data.status)
-
-    return str(encrypted_data)
+    # encrypted_data = CONFIG.GPG.encrypt(data_bytes, recipients=CONFIG.RECIPIENTS, always_trust=True)
+    #
+    # if encrypted_data.ok:
+    #     logger.info("Successfully encrypted output")
+    # else:
+    #     logger.error("Failed to encrypt output")
+    #     logger.error(encrypted_data.status)
+    #
+    # return str(encrypted_data)
+    return data_bytes.decode()
