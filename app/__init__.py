779cb9b92dd1afc13183635c9a6935160144f7f8
Jon Kent <jonkent95@gmail.com>
2023-10-10 16:50:55 +0100

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from sdx_gcp.app import get_logger, SdxApp
 logger = get_logger()
 
 
-ROOT_FOLDER = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 project_id = os.getenv('PROJECT_ID', 'ons-sdx-sandbox')
 data_sensitivity = os.getenv('DATA_SENSITIVITY', 'High')
 data_recipient = os.getenv('DATA_RECIPIENT', 'dap@ons.gov.uk')
@@ -30,7 +29,7 @@ sdx_app = SdxApp("sdx-deliver", project_id)
 def setup_keys():
 
     gpg = gnupg.GPG()
-    with open(f'{ROOT_FOLDER}/sdx_public_gpg.asc') as f:
+    with open('sdx_public_gpg.asc') as f:
         key_data = f.read()
         f.close()
     gpg.import_keys(key_data)
